64f77ec5dee900076bb271438a238a2cdcaa4c59
КРУГ ЧЕРНЫЙ <lavrentiy36@mail.ru>
2026-09-23 18:16:37 +0300

    Lesson 48 - Learning days in progress

--- Blazor/Academy/Data/AcademyContext.cs
+++ Blazor/Academy/Data/AcademyContext.cs
@@ -15,5 +15,8 @@ namespace Academy.Data
         }
 
         public DbSet<Academy.Models.Discipline> Disciplines { get; set; } = default!;
+        public DbSet<Academy.Models.Direction> Directions { get; set; } = default!;
+        public DbSet<Academy.Models.Group> Groups { get; set; } = default!;
+        public DbSet<Academy.Models.Student> Students { get; set; } = default!;
     }
 }
---
+++ Blazor/Academy/Models/Human.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Academy.Models
+{
+	public class Human
+	{
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		[RegularExpression("^[A-ZА-Я][a-zа-я]+$")]
+		public string last_name { get; set; }
+
+		[Required]
+		[StringLength(50, MinimumLength = 2)]
+		public string first_name { get; set; }
+
+		public string? middle_name { get; set; }
+
+		[Required]
+		[DataType(DataType.Date)]
+		public DateOnly birth_date { get; set; }
+
+		//[EmailAddress]
+		//[Required(AllowEmptyStrings = true)]
+		public string? email { get; set; }
+
+		//[Phone]
+		//[Required(AllowEmptyStrings = true)]
+		public string? phone { get; set; }
+
+		[Column("photo", TypeName = "IMAGE")]
+		public byte[]? photo { get; set; }
+
+		//			Calculated properties:
+		public string FullName
+		{
+			get => $"{last_name} {first_name} {middle_name}";
+		}
+	}
+}
